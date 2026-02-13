733a61b4b51baf6b9f2be8d3901fa13e0e8f05ba
ioly Oliveira <iolymmoliveira@gmail.com>
2026-02-13 17:54:10 -0300

    feat: Add GET client by id and exception handling

--- desafio03/src/main/java/com/iolyOliveira/desafio03/services/ClientService.java
+++ desafio03/src/main/java/com/iolyOliveira/desafio03/services/ClientService.java
@@ -3,12 +3,12 @@ package com.iolyOliveira.desafio03.services;
 import com.iolyOliveira.desafio03.DTO.ClientDTO;
 import com.iolyOliveira.desafio03.entities.Client;
 import com.iolyOliveira.desafio03.repositories.ClientRepository;
+import com.iolyOliveira.desafio03.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 
 @Service
 public class ClientService {
@@ -22,4 +22,10 @@ public class ClientService {
         return result.map(ClientDTO::new);
     }
 
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Client client = clientRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
+        return new ClientDTO(client);
+    }
 }
