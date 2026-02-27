8a21de7bb4f0299f78e0983b352751983a72e0b0
ioly Oliveira <iolymmoliveira@gmail.com>
2026-02-27 17:24:56 -0300

    refactor: improve validations

--- desafio05/src/main/java/com/iolyoliveira/desafio05/dto/ValidationError.java
+++ desafio05/src/main/java/com/iolyoliveira/desafio05/dto/ValidationError.java
@@ -17,6 +17,7 @@ public class ValidationError extends CustomError {
     }
 
     public void addError(String fieldName, String message) {
+        errors.removeIf(x -> x.getFieldName().equals(fieldName));
         this.errors.add(new FieldMessage(fieldName, message));
     }
 }
